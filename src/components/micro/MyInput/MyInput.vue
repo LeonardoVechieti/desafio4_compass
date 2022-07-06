a0a0d9c31ed9c201652d086076f5f69a4f515fbd
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-07-06 15:41:34 -0300

    Feat: Adding component input

---
+++ src/components/micro/MyInput/MyInput.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <input
+      :type="Type"
+      :class="classInputs"
+      :id="idInputs"
+      :placeholder="placeholder"
+      :value="value"
+    />
+  </div>
+</template>
+<script>
+
+export default {
+  name: "MyInput",
+  data() {
+    
+  },
+
+  props: {
+    Type: {
+      type: String,
+      default: "text",
+    },
+    classInputs: {
+      type: String,
+      default: "inputs",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    idInputs: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+  
+};
+</script>
+<style lang="sass" scoped>
+@import './MyInput.scss'
+</style>
