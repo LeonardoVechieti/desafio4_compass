f3e3c9f1c98519bcdae10561b0829725feaf9c52
gsprestes~ <guilherme26prestes@gmail.com>
2022-07-08 14:39:00 -0300

    feat: button folder

---
+++ src/components/micro/Button/ButtonComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <button :class="classButton">{{ text }}</button>
+</template>
+
+<script>
+
+export default {
+    name: "ButtonComponent",
+    props: {
+        text: {
+            type: String,
+            default: ""
+        },
+        classButton: {
+            type: String
+        },
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './ButtonComponent.scss';
+</style>
