f68c4678ed4e5abc103ba4f35b064285a6b6fe3f
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-07-12 22:00:44 -0300

    Feat: social-page

--- src/components/containers/Tabs/Social/Social.vue
+++ src/components/containers/Tabs/Social/Social.vue
@@ -1,15 +1,40 @@
 <template>
-  <p>ola pagina Social</p>
+  <div class="card-social">
+    <div class="forms">
+      <Label labelName="LinkedIn" />
+      <Input
+        type="text"
+        idInputs="linkedln"
+        placeholder="https://www.linkedin.com/in/foo-bar-3a0560104/"
+        class="input"
+      />
+    </div>
+    <div class="forms">
+      <Label labelName="Github *" />
+      <Input
+        type="text"
+        idInputs="github"
+        placeholder="https://github.com/foobar"
+        class="input"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import Label from "@/components/micro/Label/Label.vue";
+import Input from "@/components/micro/Input/Input.vue";
 
 export default {
-// eslint-disable-next-line
-name: "Social",
-}
+  // eslint-disable-next-line
+  name: "Social",
+  components: {
+    Label,
+    Input,
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss" scooped>
+@import "./Social.scss";
 </style>
