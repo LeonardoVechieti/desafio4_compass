6176ffeab25586329388fc1e14620fcac5b0d526
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-07-17 16:46:17 -0300

    Feat: adding content to success

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Form from '../views/Form/Form.vue'
 import Error404 from '../views/Error404/Error404.vue'
 import Error401 from '../views/Error401/Error401.vue'
+import Success from '../components/containers/Tabs/Success/Success.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,11 @@ const routes = [
     path: "/401",
     name: "Error 401",
     component: Error401,
+  },
+  {
+    path: "/success",
+    name: "Success",
+    component: Success,
   }
   
 ]
